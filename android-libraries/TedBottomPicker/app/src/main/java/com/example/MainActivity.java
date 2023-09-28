8858d55c49e5b8a303dbd6f0f26eab25a99444db
cuongnh3 <cuongnh3@rikkeisoft.com>
2023-09-28 17:39:58 +0700

    update

---
+++ android-libraries/TedBottomPicker/app/src/main/java/com/example/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.normal.TedPermission;
+
+import java.io.IOException;
+import java.util.List;
+
+import gun0912.tedbottompicker.TedBottomPicker;
+
+
+public class MainActivity extends AppCompatActivity {
+    Button btnSelectPhoto;
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnSelectPhoto = findViewById(R.id.btn_select_photo);
+        imageView = findViewById(R.id.image_view);
+        btnSelectPhoto.setOnClickListener(view -> requestPermissions());
+
+    }
+
+    private void requestPermissions() {
+        PermissionListener permissionlistener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                Toast.makeText(MainActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
+//                new TedBottomPicker.Builder(MainActivity.this)
+//                        .setOnImageSelectedListener(uri -> {
+//                        })
+//                        .create().show(getSupportFragmentManager());
+                TedBottomPicker.with(null)
+                        .show(uri -> {
+                            try {
+                                imageView.setImageBitmap(MediaStore.Images.Media.getBitmap(getContentResolver(), uri));
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        });
+            }
+
+            @Override
+            public void onPermissionDenied(List<String> deniedPermissions) {
+                Toast.makeText(MainActivity.this, "Permission Denied\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+            }
+        };
+        TedPermission.create()
+                .setPermissionListener(permissionlistener)
+                .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
+                .setPermissions(android.Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE)
+                .check();
+    }
+}
