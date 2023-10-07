0c392a5e3b3e04e1456e3f62ffa22853d6248ab5
cuongnh2k <cuongnh2k@gmail.com>
2023-10-08 06:04:24 +0700

    update

---
+++ android-libraries/ButterKnife/app/src/main/java/com/example/butterknife/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.butterknife;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btnn_1)
+    Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+}
