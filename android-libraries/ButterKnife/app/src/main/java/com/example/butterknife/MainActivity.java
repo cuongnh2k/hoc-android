926018308c856b036f628ebc8b2e92bf01665141
cuongnh2k <cuongnh2k@gmail.com>
2023-10-08 14:22:10 +0700

    update

--- android-libraries/ButterKnife/app/src/main/java/com/example/butterknife/MainActivity.java
+++ android-libraries/ButterKnife/app/src/main/java/com/example/butterknife/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.butterknife;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +21,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btnn_1, R.id.btnn_2})
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnn_1:
+                Toast.makeText(this, "Button 1", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnn_2:
+                Toast.makeText(this, "Button 2", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
